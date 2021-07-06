fefdd5bc4f38c9e24b4b192a637d930d9b4522ee
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-07-06 22:19:29 +0300

    Updates

---
+++ precache-manifest.23b3a3bbfde6a65339d5112079b30fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9cac060eeea38d9169ea0e3db39e2c",
+    "url": "/todolist-incubator-ts/index.html"
+  },
+  {
+    "revision": "e9ca33cf9c863734ed1f",
+    "url": "/todolist-incubator-ts/static/css/main.7a6bf93a.chunk.css"
+  },
+  {
+    "revision": "c3aded9c49574948935a",
+    "url": "/todolist-incubator-ts/static/js/2.db5a7b06.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist-incubator-ts/static/js/2.db5a7b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ca33cf9c863734ed1f",
+    "url": "/todolist-incubator-ts/static/js/main.1cf18c93.chunk.js"
+  },
+  {
+    "revision": "1672a915eaf4d7a39cf6",
+    "url": "/todolist-incubator-ts/static/js/runtime-main.6ca8abb4.js"
+  }
+]);
