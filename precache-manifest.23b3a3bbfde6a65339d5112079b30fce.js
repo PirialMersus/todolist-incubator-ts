b2fbd8c40cfd5c01388a93b8c227b53fede325aa
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-07-06 22:42:13 +0300

    Updates

--- precache-manifest.23b3a3bbfde6a65339d5112079b30fce.js
+++ precache-manifest.23b3a3bbfde6a65339d5112079b30fce.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa229087bf18a6552bcb80942dd7f5cd",
+    "revision": "fc9cac060eeea38d9169ea0e3db39e2c",
     "url": "/todolist-incubator-ts/index.html"
   },
   {
-    "revision": "e878fcf031aa6407b9f2",
+    "revision": "e9ca33cf9c863734ed1f",
     "url": "/todolist-incubator-ts/static/css/main.7a6bf93a.chunk.css"
   },
   {
-    "revision": "d0b95834998647538c46",
-    "url": "/todolist-incubator-ts/static/js/2.2922effe.chunk.js"
+    "revision": "c3aded9c49574948935a",
+    "url": "/todolist-incubator-ts/static/js/2.db5a7b06.chunk.js"
   },
   {
     "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
-    "url": "/todolist-incubator-ts/static/js/2.2922effe.chunk.js.LICENSE.txt"
+    "url": "/todolist-incubator-ts/static/js/2.db5a7b06.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e878fcf031aa6407b9f2",
-    "url": "/todolist-incubator-ts/static/js/main.34c0b768.chunk.js"
+    "revision": "e9ca33cf9c863734ed1f",
+    "url": "/todolist-incubator-ts/static/js/main.1cf18c93.chunk.js"
   },
   {
     "revision": "1672a915eaf4d7a39cf6",
